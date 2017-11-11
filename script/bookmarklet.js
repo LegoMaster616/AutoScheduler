bbd69c7f005ebb496f9bb9216c015ea9f2e6b48a
Andrew Afflitto <andrew@undeadpenguin.com>
2017-11-11 03:29:35 -0500

    add parse url function to get a list of call numbers from scheduler URL

--- script/bookmarklet.js
+++ script/bookmarklet.js
@@ -1,3 +1,24 @@
+/* General bookmarklet script:
+javascript:(
+function(){
+  var courses = [];
+  var inputs = document.getElementsByName('Callnum'); //contains all 10 Add class inputs but also hidden callnums for classes already registered
+  var courseIndex = 0;
+
+  for(var i = 0; i < inputs.length && courseIndex < courses.length; i++)
+  {
+    if(inputs[i].getAttribute('type') == 'text') //need to make sure it's a text input and not a hidden callnum field
+    {
+      inputs[i].value = courses[courseIndex++];
+    }
+  }
+  document.forms['StuForm'].submit(); //optional line to submit form once call numbers are copied
+})();
+
+Condensed into one line:
+javascript:(function(){var courses=[];var inputs=document.getElementsByName('Callnum');var courseIndex=0;for(var i=0;i<inputs.length&&courseIndex<courses.length;i++){if(inputs[i].getAttribute('type')=='text'){inputs[i].value=courses[courseIndex++];}}document.forms['StuForm'].submit();})();
+*/
+
 function createBookmarklet(callNumList, autoSubmit)
 {
   var str = "javascript:(function(){var courses=[";
@@ -9,23 +30,14 @@ function createBookmarklet(callNumList, autoSubmit)
   return str
 }
 
-/* General bookmarklet script:
-javascript:(
-function(){
-  var courses = [];
-  var inputs = document.getElementsByName('Callnum');
-  var courseIndex = 0;
-
-  for(var i = 0; i < inputs.length && courseIndex < courses.length; i++)
-  {
-    if(inputs[i].getAttribute('type') == 'text')
-    {
-      inputs[i].value = courses[courseIndex++];
-    }
-  }
-  document.forms['StuForm'].submit();
-})();
-
-CONDENSED:
-javascript:(function(){var courses=[];var inputs=document.getElementsByName('Callnum');var courseIndex=0;for(var i=0;i<inputs.length&&courseIndex<courses.length;i++){if(inputs[i].getAttribute('type')=='text'){inputs[i].value=courses[courseIndex++];}}document.forms['StuForm'].submit();})();
-*/
+function parseURL(url)
+{
+  var callNumList = [];
+  var urlParams = url.split('#')[1]; //get second half of url
+  var termList = urlParams.split('&'); //split into separate terms (scheduler lets you add courses from different terms)
+  termList.forEach(function(term) {
+    var callNums = term.substr(6).split(','); //remove first 6 characters (ex: '2017S=') then split into each callnum
+    callNumList = callNumList.concat(callNums); //concat array to total callNumList array
+  });
+  return callNumList;
+}
